afc51efe090c13286995b97322b5758eb78efe74
Mdzikrialfathir <108861930+Mdzikrialfathir@users.noreply.github.com>
2022-07-15 08:51:21 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', '𝗖𝗥𝗘𝗔𝗧𝗢𝗥➢AzzBot\nSubscribe : https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g', m)
+	conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/?apikey=YogGanz', '', 'Dosa➢\nSubscribe : https://youtube.com/channel/UCmUhiytK9WMYaOFgdOho_2g', m)
 }
 handler.help = ['bkp']
 handler.tags = ['asupan']
